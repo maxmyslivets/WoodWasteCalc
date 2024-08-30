82fa11629d9663a54660c7098a4918826635b37d
maxmyslivets <by.mikky1206@gmail.com>
2024-08-30 23:16:26 +0300

    добавлена возможность завершения программы при ошибочных исходных данных без дальнейшей обработки

--- not_gui/wood_waste_calc_not_gui.py
+++ not_gui/wood_waste_calc_not_gui.py
@@ -1,29 +1,42 @@
+import sys
+
 from parsing.config import Config
 from parsing.directory import list_files_in_directory
 from parsing.parse_xls import XLSParser, RawWood
+from wood_objects.wood import Wood, Trunk
 
 config = Config()
 
 
-def main():
+def parse() -> list[Wood]:
 
     # Чтение файлов таблиц
     files_for_parse = list_files_in_directory(config.settings.parse_directory)
-    woods = []
+    raw_woods = []
     for file in files_for_parse:
         wood_of_file = XLSParser().parse(file)
-        woods.extend(wood_of_file)
+        raw_woods.extend(wood_of_file)
 
+    woods = []
     # Парсинг таблиц
-    for wood_list in woods:
+    for wood_list in raw_woods:
         raw_wood = RawWood(*wood_list)
         try:
             if raw_wood.is_valid():
                 for wood in raw_wood.parse():
-                    print(wood)
+                    woods.append(wood)
         except Exception as e:
             print(e)
 
+    # TODO: Обработка непрочитанных деревьев
+    if input("Имеются ошибки в некоторых исходных данных. Продолжать (пустой ввода, если да)?") != "":
+        sys.exit(0)
 
-if __name__ == '__main__':
-    main()
+    for wood in woods:
+        print(wood)
+
+    return woods
+
+
+def main() -> None:
+    source_woods = parse()
